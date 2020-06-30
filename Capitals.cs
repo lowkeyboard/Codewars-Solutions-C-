fe1a8bef3902af9affbc4ae056f6109f30d93a9b
cadev <46297574+lowkeyboard@users.noreply.github.com>
2020-06-30 15:33:00 +0300

    Add files via upload

---
+++ Capitals.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+
+namespace arraying
+{
+    class Program
+    {
+
+
+        public static int[] Capitals(string word)
+        {
+            char[] chars = word.ToCharArray(); // 'S','p','o','T',.. chars[]
+
+            List<int> someList = new List<int>();
+
+
+
+            for (int i = 0; i < chars.Length; i++)
+            {
+                bool result = Char.IsUpper(word, i);
+                if (result is true)
+                {
+                    someList.Add(i);
+                }
+            }
+
+
+            int[] capitals = someList.ToArray();
+
+
+
+            return capitals;
+
+        }
+
+        static void Main(string[] args)
+        {
+            string word = "SpoTifY";
+            Capitals(word);
+
+
+        }
+    }
+}
